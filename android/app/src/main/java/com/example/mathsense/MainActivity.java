73b70077b048b30c83ab301175bfc9035b5c395a
Varad Gundap <30592638+varad615@users.noreply.github.com>
2024-07-09 21:18:58 +0530

    updated code

---
+++ android/app/src/main/java/com/example/mathsense/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.mathsense;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/mathsense/app/mathsense/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mathsense.app.mathsense
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
